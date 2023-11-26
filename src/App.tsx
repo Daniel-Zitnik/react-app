c8525937d23897e65b8083c94684e74f57032628
Zitnik <zitnik.cz@gmail.com>
2023-11-26 23:35:11 +0100

    added react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './assets/App.scss';
+import { Route, NavLink, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/tunes">Tunes</NavLink>
+        <NavLink to="/about">About</NavLink>
       </header>
+
+      <main>
+        <Routes>
+          <Route path='/' />
+          <Route path='/tunes' />
+          <Route path='/about' />
+        </Routes>
+      </main>
+
+      <footer>
+
+      </footer>
     </div>
   );
 }
