f4818c70283dc62029c69dba40885443495421c4
Zitnik <zitnik.cz@gmail.com>
2023-11-30 16:11:40 +0100

    counter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
 // styles
-import styles from '../styles/Home.module.scss'
-import logo from '../assets/tunes.png'
+import styles from '../styles/Home.module.scss';
+import logo from '../assets/tunes.png';
 
 type Props = {}
 
 const Home = (props: Props) => {
+    const [count, setCount] = useState(0);
+
     return (
         <>
         <h1>Home</h1>
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" onClick={() => {
+            setCount (prev => prev + 1);
+        }} />
+        <p>{count}</p>
         </>
      )
 }
