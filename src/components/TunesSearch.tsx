61445aafeea6ff84e12871a9c97ef52ff79f44d8
Zitnik <zitnik.cz@gmail.com>
2023-12-09 15:09:45 +0100

    code simplification

--- src/components/TunesSearch.tsx
+++ src/components/TunesSearch.tsx
@@ -1,34 +1,37 @@
 // react
-import React, { useState, ChangeEvent, FormEvent } from 'react'
+import React, { useRef, ChangeEvent, FormEvent } from 'react'
 // styles
 import styles from '../styles/Tunes.module.scss';
 
 type Props = {
     onSearchFormSubmit: (data: string) => void
-    onInputChange: (data: string) => void
-    searchQuery: string
 }
 
 const TunesSearch = (props: Props) => {
-    // props
-    const { searchQuery } = props
+    // input ref
+    const searchInput = useRef<HTMLInputElement>(null)
 
     // input change
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        props.onInputChange(e.target.value);
+        searchMusic();
     }
 
     // form submit
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        props.onSearchFormSubmit(searchQuery);
+        searchMusic();
+    }
+
+    // search music
+    const searchMusic = () => {
+        console.log(searchInput.current?.value);
     }
 
     // template
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" value={searchQuery} onChange={handleInputChange} />
+                <input type="text" ref={searchInput} autoFocus onChange={handleInputChange}  />
             </form>
         </div>
     )
--- src/pages/Tunes.tsx
+++ src/pages/Tunes.tsx
@@ -10,27 +10,11 @@ type Props = {}
 
 const Tunes = (props: Props) => {
     // state
-    const [searchQuery, setSearchQuery] = useState('');
-    const [songs, setSongs] = useState([
-        {id: 1, artist: 'one', name: 'one song'},
-        {id: 2, artist: 'two', name: 'two song'},
-        {id: 3, artist: 'three', name: 'three song'}
-    ])
-
-    // callback input change
-    const handleInputChange = (data: string) => {
-        setSearchQuery(data);
-    }
+    const [songs, setSongs] = useState([])
 
     // callback form submit
     const handleSearchFormSubmit = (data: string) => {
-        const newSong = {
-            id: Math.max (...songs.map (s => s.id)) + 1,
-            artist: data,
-            name: data + ' song'
-        }
-
-        setSongs([...songs, newSong]);
+        setSongs([]);
     }
 
     // template
@@ -39,8 +23,6 @@ const Tunes = (props: Props) => {
         <h1>tunes</h1>
         <TunesSearch 
             onSearchFormSubmit={handleSearchFormSubmit} 
-            onInputChange={handleInputChange} 
-            searchQuery={searchQuery}
         />
         <TunesList songs={ songs }/>
         </>
