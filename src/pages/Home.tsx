e6356186fee989dc642ca59935017e8cd317126d
Zitnik <zitnik.cz@gmail.com>
2023-12-17 13:30:31 +0100

    added react logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 // styles
-import styles from '../styles/Home.module.scss';
-import logo from '../assets/tunes.png';
+import '../styles/Home.scss';
+import logo from '../assets/logo.svg';
 
 type Props = {}
 
@@ -9,13 +9,13 @@ const Home = (props: Props) => {
     const [count, setCount] = useState(0);
 
     return (
-        <>
-        <h1>Home</h1>
-        <img src={logo} alt="logo" onClick={() => {
-            setCount (prev => prev + 1);
-        }} />
-        <p>{count}</p>
-        </>
+        <div className='container home'>
+            <h1>Home</h1>
+            <img className="logo" src={logo} alt="react logo" onClick={() => {
+                setCount (prev => prev + 1);
+            }} />
+            <p>{count}</p>
+        </div>
      )
 }
 
